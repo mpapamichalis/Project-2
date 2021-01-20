d94d1ab62f95ade0dfb302ae47ad8d82bd943f42
Seth Staggs <sethstaggs2@gmail.com>
2021-01-19 19:23:21 -0600

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,14 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Samm@b0y",
-    database: "blood_db"
+const connection = mysql.createConnection({host: "localhost",port: 3306,user: "root",password: "Samm@b0y",database: "blood_db"
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connectiong: " + err.stack);
-        return;
-    }
-    console.log("connected as id "+ connection.threadId);
+connection.connect((err) => {
+  if (err) {
+    console.error("error connectiong: " + err.stack);
+    return;
+  }
+  console.log("connected as id "+ connection.threadId);
 });
 
 module.exports = connection;
