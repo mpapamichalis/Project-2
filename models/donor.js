25d8f3345065b92ad6a569dfb04f7b2e595bac07
Seth Staggs <sethstaggs2@gmail.com>
2021-01-21 20:53:31 -0600

    seeded database

--- models/donor.js
+++ models/donor.js
@@ -1,31 +1,29 @@
-// module.exports = function (sequelize, DataTypes) {
-//   const Donor = sequelize.define("Donor", {
-//     donorName: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     bloodType: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     canDonate: {
-//       type: DataTypes.BOOLEAN,
-//       default: true
-//     }
-//   });
-//   return Donor;
-// };
+module.exports = function (sequelize, DataTypes) {
+  const Donor = sequelize.define("Donor", {
+    donorName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    bloodType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    canDonate: {
+      type: DataTypes.BOOLEAN,
+      default: true
+    }
+  });
+  return Donor;
+};
 
-const Sequelize = require("sequelize");
-const sequelize = require("../config/connection.js");
+// const Sequelize = require("sequelize");
+// const sequelize = require("../config/connection.js");
 
 
-const Donor = sequelize.define("donor", {
-  donorName: Sequelize.STRING,
-  bloodType: Sequelize.STRING,
-  canDonate: Sequelize.BOOLEAN
-});
+// const Donor = sequelize.define("donor", {
+//   donorName: Sequelize.STRING,
+//   bloodType: Sequelize.STRING,
+//   canDonate: Sequelize.BOOLEAN
+// });
 
-Donor.sync();
-
-module.exports = Donor;
+// Donor.sync();
