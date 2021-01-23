62667a211a7063057e71f52cef1f6dba7bba8e37
Seth Staggs <sethstaggs2@gmail.com>
2021-01-23 13:14:41 -0600

    fixed button

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ const newPat = document.getElementById("patadd-btn");
 newPat.addEventListener("click", function() {
   console.log("new Patient added");
   
-  $.ajax({url: "/api/newPatient", method: "POST", data: {patientName: "Tyler", bloodType: "AB", patientCon: "NA"}}).then(function(res) {
+  $.ajax({url: "/api/newPatient", method: "POST", data: {patientName: "Tyler", bloodType: "AB", patientCon: "NA"}}).then(function() {
     window.reload();
   });
 }); 
