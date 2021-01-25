df01352287ae47e413e307d19173cc9c5952efe7
Seth Staggs <sethstaggs2@gmail.com>
2021-01-25 16:47:34 -0600

    pre-login page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-arrow-callback */
 const newPat = document.getElementById("patadd-btn");
-
+const newDon = document.getElementById("adddonor-btn");
 
 newPat.addEventListener("click", function() {
   console.log("new Patient added");
@@ -8,4 +8,15 @@ newPat.addEventListener("click", function() {
   $.ajax({url: "/api/newPatient", method: "POST", data: {patientName: "Tyler", bloodType: "AB", patientCon: "NA"}}).then(function() {
     window.reload();
   });
+
 }); 
+
+$("#adddonor-btn").on("click", function(event) {
+  event.preventDefault();
+  console.log("new Donor added");
+    
+  $.ajax({url: "/api/newDonor", method: "POST", data: {donorName: "Steven Douglas", bloodType: "O-", canDonate: true}}).then(function() {
+    window.reload();
+  });
+}); 
+  
